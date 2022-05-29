243ab98e458f77d53103682f8eebd36bc08a879e
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-05-29 16:11:45 +0100

    Organise entry point & add comments

--- source/main.js
+++ source/main.js
@@ -1,19 +1,36 @@
+// Import all request routes
 import { requestRoutes } from "./routes"
 
+// Returns a promise that resolves to an empty response with the provided HTTP status code
+const emptyResponse = async ( statusCode ) => {
+	return new Response( null, { status: statusCode } )
+}
+
+// Handle incoming Worker requests...
 addEventListener( "fetch", ( event ) => {
-	const availableRoutes = requestRoutes.get( event.request.method )
-	if ( !availableRoutes ) return new Response( null, { status: 405 } )
 
+	// Parse the full URL
 	const requestUrl = new URL( event.request.url )
-	const destinationRoute = availableRoutes.get( requestUrl.pathname )
-	if ( !destinationRoute ) return new Response( null, { status: 501 } )
 
+	// Get the routes available for this method
+	const availableRoutes = requestRoutes.get( event.request.method )
+
+	// Respond with method not allowed if there are no routes for this method
+	if ( !availableRoutes ) return event.respondWith( emptyResponse( 405 ) )
+	
+	// Get the route function for this path from the routes for this method
+	const destinationRoute = availableRoutes.get( requestUrl.pathname )
+
+	// Respond with not implemented if there is no route for this path
+	if ( !destinationRoute ) return event.respondWith( emptyResponse( 501 ) )
+
+	// Execute the route function and store the response
 	const routeResponse = destinationRoute( event.request, event )
-	if ( routeResponse ) {
-		event.respondWith( routeResponse )
-	} else {
-		event.respondWith( new Promise( ( resolve ) => {
-			resolve( new Response( null, { status: 204 } ) )
-		} ) )
-	}
+
+	// Respond with no content if the route did not give a response
+	if ( !routeResponse ) return event.respondWith( emptyResponse( 204 ) )
+
+	// Respond with whatever the route returned
+	event.respondWith( routeResponse )
+
 } )
--- source/types.js
+++ source/types.js
@@ -1,14 +1,16 @@
+// This should not contain all enumerations, only the ones that are used.
+
+// https://discord.com/developers/docs/resources/channel#message-object-message-flags
 export const MessageFlags = {
-	SuppressEmbeds: 1 << 2,
 	Ephemeral: 1 << 6
 }
 
+// https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
 export const ApplicationCommandTypes = {
-	ChatInput: 1,
-	User: 2,
-	Message: 3
+	ChatInput: 1
 }
 
+// https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
 export const ApplicationCommandOptionTypes = {
 	SubCommand: 1,
 	SubCommandGroup: 2,
@@ -19,23 +21,26 @@ export const ApplicationCommandOptionTypes = {
 	Channel: 7,
 	Role: 8,
 	Mentionable: 9,
-	Double: 10,
+	Number: 10,
 	Attachment: 11
 }
 
+// https://discord.com/developers/docs/interactions/application-commands#application-command-permissions-object-application-command-permission-type
 export const ApplicationCommandPermissionTypes = {
 	Role: 1,
-	User: 2
+	User: 2,
+	Channel: 3
 }
 
+// https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-interaction-type
 export const InteractionTypes = {
 	Ping: 1,
 	ApplicationCommand: 2,
 	MessageComponent: 3,
-	ApplicationCommandAutocomplete: 4,
-	ModalSubmit: 5
+	ApplicationCommandAutocomplete: 4
 }
 
+// https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-response-object-interaction-callback-type
 export const InteractionCallbackTypes = {
 	Pong: 1,
 	ChannelMessageWithSource: 4,
